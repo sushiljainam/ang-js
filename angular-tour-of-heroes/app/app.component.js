d7bfa4c4bbf52c61f4d991050501f1107aaa4cf1
sushiljainam <sushiljain1993@gmail.com>
2016-10-14 15:49:34 +0530

    new proj init as ng start

--- angular-tour-of-heroes/app/app.component.js
+++ angular-tour-of-heroes/app/app.component.js
@@ -7,13 +7,4 @@
 		.Class({
 			constructor: function() {}
 		});
-	// create a login component
-	app.loginAppComponent =
-		ng.core.Component({
-			selector: 'login-app',
-			template: '<h1>My Login App</h1>'
-		})
-		.Class({
-			constructor: function() {}
-		});
 })(window.app || (window.app = {}));
--- angular-tour-of-heroes/app/app.module.js
+++ angular-tour-of-heroes/app/app.module.js
@@ -2,8 +2,8 @@
 	app.AppModule =
 		ng.core.NgModule({
 			imports: [ ng.platformBrowser.BrowserModule ],
-			declarations: [ app.AppComponent, app.loginAppComponent ],
-			bootstrap: [ app.AppComponent, app.loginAppComponent ]
+			declarations: [ app.AppComponent ],
+			bootstrap: [ app.AppComponent ]
 		})
 		.Class({
 			constructor: function() {}
